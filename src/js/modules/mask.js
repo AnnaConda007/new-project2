340048c61426e1e994552d20e611927b81c4bfc5
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-18 18:52:13 +0400

    сокращено

--- src/js/modules/mask.js
+++ src/js/modules/mask.js
@@ -1,49 +1,82 @@
+
+const mask = (selector) => {
+    function createMask(event) {
+        let sample = `+7 (___) ___ __ __`;
+        let i = 0;
+        let emptyInput = sample.replace(/\D/g, ""); //значение этой переменной никогда не меняется. В консоли она всегда равна +7
+        let value = this.value.replace(/\D/g, "");// this берет данные из input,и сохраняет в себя только цифры. При каждом новом запуске функций, переменная обращается к input  в браузере
+
+        if (emptyInput.length >= value.length) {// emptyInput всегда равен 7. emptyInput.length>=value.length только тогда когда пользователь еще не введ никаких символов. 
+            value = emptyInput;// поэтому value[0] принимает значение 7. 
+
+        }
+
+        this.value = sample.replace(/./g, function (a) {//результат проверки возвращается непосредственно в input, а не в переменную 
+            if (/[_\d]/.test(a) && i < value.length) {//" [_\d]" указывает, что нужно найти "_" и заменить на то, что является цифрой.
+                //==>test(a) проверяет, является ли а цифрой. Если да, то возвращает true. ==>   ?i < value.length?
+                console.log("if i < value.length, то i =" + i)
+                return value.charAt(i++)// если условие выполнено, то "а"  возвращается  в в input  на место равное i++
+            } else {
+                console.log("i >= value.length, то i =" + i)
+                console.log("________ ")
+                if (i >= value.length) { return "" } else { return a }
+
+            }
+        }
+        );
+
+    }
+    let input = document.querySelectorAll(selector);
+    input.forEach((input) => {
+        input.addEventListener("input", createMask);
+        input.addEventListener("focus", createMask);
+
+    });
+};
+
+export default mask;
+
+/*
 const mask = (selector) => {
     let setCursorPosition = (pos, elem) => {
         elem.focus();
         if (elem.setSelectionRange) {
-            // если браузер поодерживает setSelectionRange
-            elem.setSelectionRange(pos, pos); // ставим курсор в нужное место
+
+            elem.setSelectionRange(pos, pos);
         } else if (elem.createTextRange) {
             let range = elem.createTextRange();
 
-            range.collapse(true); // объеденяем диапозон
-            range.moveEnd("character", pos); // конечная точка
-            range.moveStart("character", pos); // начальная точка
-            range.select(); // выделяем выбранный диапозон, то есть ставим курсор
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select();
         }
     };
 
     function createMask(event) {
         let sample = `+7 (___) ___ __ __`;
-        let i = 0; // при каждом запуске функции, то есть при введении нового знака, переменная снова =0
-        let defaultValue = sample.replace(/\D/g, ""); // изначальное значение до ввода  всех данных - все кроме цифр =, как только введен символ, он сохраняется в value
-        let value = this.value.replace(/\D/g, ""); // уже введенные данные,обновляется при каждом запуске функций, все кроме цифр равно пустой строке
+        let i = 0;
+        let emptyInput = sample.replace(/\D/g, "");
+        let value = this.value.replace(/\D/g, "");
 
-        if (defaultValue.length >= value.length) {
-            //??зачем это нужно 
-            // я поняла эту часть так - если длина первоначального значения больше нового введенного значения, то в input остается
-            // первоначальное значение, то есть, например, в input уже введено +7 85_ ___ __ __ , вводим "5". defaultValue = +7 85_ ___ __ __ 
-            //value = 5.  defaultValue.length >= value.length. Теперь value = +7 85_ ___ __ __, но нывый символ "5", тогда не вносится
-            value = defaultValue;
+        if (emptyInput.length >= value.length) {
+            console.log("emptyInput: " + emptyInput)
+            console.log("value до : " + value)
+            value = emptyInput;
+            console.log("value после : " + value)
         }
-        562;
+
         this.value = sample.replace(/./g, function (a) {
-            //проверяется каждый знак в value
             return /[_\d]/.test(a) && i < value.length
                 ? value.charAt(i++)
                 : i >= value.length
                     ? ""
                     : a;
-        }); /*  "а" проверятеся на соответсвтие рег выражению - [только _ и цифры]
-        и если i меньше кооличества введенных символов, то возвращается из строки(уже введенных цифр) символ с индексом i++ , но i всегда =0 (19 строка)  =>(value.charAt(i++)),
-        если нет, то просиходит еще одна проверка, на то что i >=колличества введенных символов, если проверка прошла успешно, то возвращается введенное число, если нет, то ""
-        */
+        });
         if (event.type === "blur") {
-            // если убрали мышь
+
             if (this.value.length == 2) {
-        // если введен 1 знак(то есть символ с индексом 2), предпологается, что +7 имеют индекс 0;1
-        /*? */ this.value = ""; // то очищаем поле (почему сохранятся +7  ?)
+                this.value = "";
             }
         } else {
             setCursorPosition(this.value.length, this);
@@ -51,10 +84,11 @@ const mask = (selector) => {
     }
     let input = document.querySelectorAll(selector);
     input.forEach((input) => {
-        input.addEventListener("input", createMask); // функция запускается при любом изменени в input
-        input.addEventListener("focus", createMask); // и при каждом наведении(клике)
-        input.addEventListener("blur", createMask); // при клике вне формы
+        input.addEventListener("input", createMask);
+        input.addEventListener("focus", createMask);
+        input.addEventListener("blur", createMask);
     });
 };
 
 export default mask;
+*/
