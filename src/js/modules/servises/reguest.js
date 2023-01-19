adc6f3859b31ad2f3b1ce909993bc93555e15aac
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-19 18:14:15 +0400

    подгружаются данные из json

---
+++ src/js/modules/servises/reguest.js
@@ -0,0 +1,10 @@
+const getResponse = async (url) => {
+    let response = await fetch(url)
+
+    if (!response.ok) {
+      /*??*/  throw new Error(`Could not fetch ${url}, status: ${response.status}`)
+    }
+    return await response.json()
+}
+
+export { getResponse }
---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,38 @@
+import { getResponse } from "./servises/reguest"
+const showMoreStyles = (triger, wrapper) => {
+    const btnMoreStyle = document.querySelector(triger)
+
+
+    btnMoreStyle.addEventListener("click", function () {
+        getResponse("assets/db.json")
+            .then(responseAfterFetch => createCards(responseAfterFetch.styles))
+        this.remove()
+    })
+
+
+    function createCards(res) {
+        res.forEach(({ src, title, link }) => {
+            let card = document.createElement("div")
+            /*  card.classList.add("animated", "fedeInUp", "col-sm-3", "col-sm-offset-0", "col-xs-10", "col-xs-offset-1")
+              card.innerHTML = `
+      <div class=styles-block>
+          <img src=${src} alt>
+          <h4>${title}</h4>
+          <a href=${link}>Подробнее</a>
+      </div>
+      `
+      */card.style.backgroundColor = "red"
+            console.log(typeof (card))
+            document.querySelector(wrapper).appendChild(card)
+
+        });
+
+    }
+
+
+
+
+
+
+}
+export default showMoreStyles
