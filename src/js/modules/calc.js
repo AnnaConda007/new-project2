d9eb253f1d86539dd510ca0daadccf9a8b49ec68
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-20 12:17:44 +0400

    работает корректно

--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -1,35 +1,31 @@
 const calc = (size, material, option, promo, result) => {
-    const sizeBlock = document.querySelector(size)
-    const materialBlock = document.querySelector(material)
-    const optionBlock = document.querySelector(option)
-    const promocodeBlock = document.querySelector(promo)
-    const resultlBlock = document.querySelector(result)
+    const sizeBlock = document.querySelector(size);
+    const materialBlock = document.querySelector(material);
+    const optionBlock = document.querySelector(option);
+    const promocodeBlock = document.querySelector(promo);
+    const resultlBlock = document.querySelector(result);
 
-    let sum = 0
+    let sum = 0;
 
     const calcFunc = () => {
-        sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionBlock.value))
+        sum = Math.round(
+            +sizeBlock.value * +materialBlock.value + +optionBlock.value
+        );
 
-        if (sizeBlock.value == "" || materialBlock == "") { // не выполняется проверка на sizeBlock.value == ""
-            console.log("sizeBlock.value " + sizeBlock.value)
-            resultlBlock.textContent = "Пожалуйста, выберите размер и материал картины";
+        if (sizeBlock.value == "" || materialBlock.value == "") {
+            resultlBlock.textContent =
+                "Пожалуйста, выберите размер и материал картины";
         } else if (promocodeBlock.value === "промокод") {
-            console.log("sizeBlock.value " + sizeBlock.value)
-            resultlBlock.textContent = Math.round(sum * 0.7)
+            resultlBlock.textContent = Math.round(sum * 0.7);
         } else {
-            console.log("sizeBlock.value " + sizeBlock.value)
-            resultlBlock.textContent = sum
+            resultlBlock.textContent = sum;
         }
-    }
+    };
 
-    sizeBlock.addEventListener("change", calcFunc)
-    materialBlock.addEventListener("change", calcFunc)
-    optionBlock.addEventListener("change", calcFunc)
-    promocodeBlock.addEventListener("input", calcFunc)
+    sizeBlock.addEventListener("change", calcFunc);
+    materialBlock.addEventListener("change", calcFunc);
+    optionBlock.addEventListener("change", calcFunc);
+    promocodeBlock.addEventListener("input", calcFunc);
+};
 
-
-
-
-}
-
-export default calc
+export default calc;
