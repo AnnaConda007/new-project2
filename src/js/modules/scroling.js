6db294a0ae6dcb840c0322f0115d97ebb1b3b78d
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-25 11:50:58 +0400

    работает

--- src/js/modules/scroling.js
+++ src/js/modules/scroling.js
@@ -14,30 +14,68 @@ const scrolling = (upSelector) => {
     const body = document.body;
 
     const calcScroll = () => {
-        upElem.addEventListener("click", function (e) { // повесили обработчик на стрелку
-            let scrollTop = Math.round(body.scrollTop || element.scrollTop);    // переменая показывает сколько прокрутили
-          /*??? */  if (this.hash !== "") { // если у секции, в которой находится стрелка, хещ != "", но ведь хеш (#) задан только у header
+        upElem.addEventListener("click", function (e) {
+            // повесили обработчик на стрелку
+            let scrollTop = Math.round(body.scrollTop || element.scrollTop); // переменая показывает сколько прокрутили
+      /*??? */ if (this.hash !== "") {
+                // если у секции, в которой находится стрелка, хещ != "",            //но ведь хеш (#) задан только у header
                 e.preventDefault();
 
-                let hashElement = document.querySelector(this.hash); // получае  хеш того элемента, на котором сейчас стрелка  !!но ведь хеш (#) задан только у header
-                let hashElementTop = 0;
+                let hashElement = document.querySelector(this.hash); // получает  хеш того элемента, на который нужно перейти (header)
+
+                //  console.log(this.hash) // up
+                //  console.log(hashElement) //  header
+                //console.log(hashElement.offsetParent)   // родитель header - body
+                //console.log(hashElement.offsetTop) // растояние от текущего расположения стрелки до body
+
+                let hashElementTop = 0; // сколько нужно пролистать до родителя header(до body??)
 
                 while (hashElement.offsetParent) {
+                    // пока у header есть родитель
                     hashElementTop += hashElement.offsetTop;
-                    hashElement = offsetParent;
+                    console.log(hashElementTop);
+                    hashElement = hashElement.offsetParent;
                 }
                 hashElementTop = Math.round(hashElementTop);
-                //  smoothScroll(scrollTop, hashElement, this.hahs);
+                smoothScroll(scrollTop, hashElement, this.hahs);
             }
         });
     };
 
-    calcScroll()
+    const smoothScroll = (from, to, hash) => {
+        let timeInterval = 1;
+        let prevScrolTop;
+        let speed
+        if (to > from) {
+            speed = 30;
+        } else {
+            speed = -30;
+        }
+
+        let move = setInterval(function () {
+            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+            if (
+                prevScrolTop === scrollTop ||
+                (to > from && scrollTop >= to) ||
+                (to < from && scrollTop <= to)
+            ) {
+                clearInterval(move);
+                history.replaceState(
+                    history.state,
+                    document.title,
+                    location.href.replace(/#.*$/g, "") + hash
+                );
+            } else {
+                body.scrollTop += speed;
+                element.scrollTop += speed;
+                prevScrolTop = scrollTop;
+            }
+        }, timeInterval);
+    };
+
+    calcScroll();
 };
 export default scrolling;
-
-
-
 
 /*
 const smoothScroll = (from, to, hash) => {
