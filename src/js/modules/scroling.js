2dacf421c67d98c74fc56f1e43f2bf9e70fb19b6
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-26 13:45:55 +0400

    плавный скрол работает на всех ссылках

--- src/js/modules/scroling.js
+++ src/js/modules/scroling.js
@@ -10,7 +10,49 @@ const scrolling = (upSelector) => {
         }
     });
 
-    const element = document.documentElement;
+
+    const links = document.querySelectorAll("a");
+    links.forEach((link) => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            if (this.hash[0] === "#") {
+                const targetElemenForScroll = document.querySelector(this.hash);
+                scroll(targetElemenForScroll);
+            }
+        });
+    });
+
+    function scroll(target) {
+        target.scrollIntoView({ behavior: "smooth" });
+    }
+
+};
+export default scrolling;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const element = document.documentElement;
     const body = document.body;
 
     const calcScroll = () => {
@@ -24,47 +66,49 @@ const scrolling = (upSelector) => {
                 while (hashElement.offsetParent) {// пока у header есть родитель body - то есть всегда
                     hashElementTop += hashElement.offsetTop;//=0  ;возвращает расстояние hashElement(header) по отношению к верхней части hashElement.offsetParent(body)
                     //эта переменная всегда будет = 0, потому что возвразает растояние между границей элемента и его родителем, тогда зачем она ?  
-            /*? */ hashElement = hashElement.offsetParent;   // hashElement = body, зачем нужна эта строка? к тому же hashElement нигде похже не используется?
-                }
-                hashElementTop = Math.round(hashElementTop);// округляет ноль
-                smoothScroll(scrollTop, hashElementTop, this.hash); // scrollTop(сколько прокрутили) hashElementTop(куда перейти)  this.hash(hash стрелки(еще раз куда перейти))
-            }
-        });
-    };
-
-    const smoothScroll = (from, to, hash) => {
-        let timeInterval = 5;
-        let prevScrolTop; // сколько пискселй нужно прокурутить до header??
-        let speed // скорость скрола
-        if (to > from) { // если высота хеш элемента больше чем текущее место// эта часть не испоьзуется в этом скрипте, никогда.
-            speed = 30;
-        } else {              // если элемент с хешом расположен на меньшей высоте(в верху страницы), чем текущее положение
-            speed = -30;
-
+         hashElement = hashElement.offsetParent;   // hashElement = body, зачем нужна эта строка? к тому же hashElement нигде похже не используется?
         }
+        hashElementTop = Math.round(hashElementTop);// округляет ноль
+        smoothScroll(scrollTop, hashElementTop, this.hash); // scrollTop(сколько прокрутили) hashElementTop(куда перейти)  this.hash(hash стрелки(еще раз куда перейти))
+    }
+});
+};
 
-        let move = setInterval(function () {
-            let scrollTop = Math.round(body.scrollTop || element.scrollTop);// показывает сколько пикселей прокручено от начала страницы
-            if (
-                prevScrolTop === scrollTop ||      //  переменная prevScrolTop с 62 строки = растоянию от элемента до верха страницы, или
-                (to > from && scrollTop >= to) ||// текущее положение стрелки в пикселях больше чем верх страницы(0)  и  проскроленное растояние >= положения стрелки в пикселях
-                (to < from && scrollTop <= to) //     текущее положение стрелки в пикселях меньше чем верх страницы(0)  и  проскроленное растояние <= положения стрелки в пикселях// это на случай, если элемент к которому нужно перейти расположен вниху страницы?
-            ) {
-                clearInterval(move);  // зачем очищать интервал? Это происходит если "докрутили" до нужного элемента ?
-                history.replaceState(
-                    history.state,
-                    document.title,
-                    location.href.replace(/#.*$/g, "") + hash
-                );
-            } else {
-                body.scrollTop += speed; // увеличиваем скрол от  боди до ?стрелки? на -30(уменьшаем растояние) ? 
-                element.scrollTop += speed;// то же самое но с element
-                prevScrolTop = scrollTop;// prevScrolTop равна оставшемуся растоянию, перезаписывается 
-            }
-        }, timeInterval);
-    };
+const smoothScroll = (from, to, hash) => {
+let timeInterval = 5;
+let prevScrolTop; // сколько пискселй нужно прокурутить до header??
+let speed // скорость скрола
+if (to > from) { // если высота хеш элемента больше чем текущее место// эта часть не испоьзуется в этом скрипте, никогда.
+    speed = 30;
+} else {              // если элемент с хешом расположен на меньшей высоте(в верху страницы), чем текущее положение
+    speed = -30;
+
+}
+
+let move = setInterval(function () {
+    let scrollTop = Math.round(body.scrollTop || element.scrollTop);// показывает сколько пикселей прокручено от начала страницы
+    if (
+        prevScrolTop === scrollTop ||      //  переменная prevScrolTop с 62 строки = растоянию от элемента до верха страницы, или
+        (to > from && scrollTop >= to) ||// текущее положение стрелки в пикселях больше чем верх страницы(0)  и  проскроленное растояние >= положения стрелки в пикселях
+        (to < from && scrollTop <= to) //     текущее положение стрелки в пикселях меньше чем верх страницы(0)  и  проскроленное растояние <= положения стрелки в пикселях// это на случай, если элемент к которому нужно перейти расположен вниху страницы?
+    ) {
+        clearInterval(move);  // зачем очищать интервал? Это происходит если "докрутили" до нужного элемента ?
+        history.replaceState(
+            history.state,
+            document.title,
+            location.href.replace(/#.*$/g, "") + hash
+        );
+    } else {
+        body.scrollTop += speed; // увеличиваем скрол от  боди до ?стрелки? на -30(уменьшаем растояние) ? 
+        element.scrollTop += speed;// то же самое но с element
+        prevScrolTop = scrollTop;// prevScrolTop равна оставшемуся растоянию, перезаписывается 
+    }
+}, timeInterval);
+};
+
 
     calcScroll();
-};
-export default scrolling;
 
+
+
+*/
