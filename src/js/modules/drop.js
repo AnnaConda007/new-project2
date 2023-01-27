782369875b71c542b8e9491c73121dba56e58e0e
Anna Hrulckova <annahrulkova@yandex.ru>
2023-01-27 20:43:17 +0400

    работает без ошибок

--- src/js/modules/drop.js
+++ src/js/modules/drop.js
@@ -1,55 +1,64 @@
 const drop = () => {
-    const fileInputs = document.querySelectorAll(`[name="upload"]`)
+  const fileInputs = document.querySelectorAll(`[name="upload"]`);
 
-
-
-    [`dragenter`, `dragleave`, `dragover`, `drop`].forEach(eventType => {
-        fileInputs.forEach(input => {
-            input.addEventListener(eventType, preventDefaults, false)
-        })
+  [`dragenter`, `dragleave`, `dragover`, `drop`].forEach((eventType) => {
+    fileInputs.forEach((input) => {
+      input.addEventListener(eventType, preventDefaults, false);
     });
+  });
 
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 
+  function hightLight(item) {
+    item.closest(".file_upload").style.border = "5px solid yellow";
+    item.closest(".file_upload").style.backGroundColor = "rgba(0,0,0, .7)";
+  }
 
-    function preventDefaults(e) {
-        e.preventDefault()
-        e.stopPropagation()
+  function unHightLight(item) {
+    item.closest(".file_upload").style.border = "none";
+    if (item.closest(".calk_form")) {
+      item.closest(".file_upload").style.backGroundColor = "white";
+    } else {
+      item.closest(".file_upload").style.backGroundColor = "ededed";
     }
-    /*
-        function hightLight(item) {
-            item.closest(".file_upload").style.border = "5px solid yelow"
-            item.closest(".file_upload").style.backGroundColor = "rgba(0,0,0, .7)"
-        }
-    
-        function unHightLight(item) {
-            item.closest(".file_upload").style.border = "none"
-            item.closest(".file_upload").style.backGroundColor = "white"
-        }
-    
-    
-    
-    
-    
-        ["dragenter", "dragover"].forEach(eventType => {
-            fileInputs.forEach((input) => {
-                input.addEventListener(eventType, () => {
-                    hightLight(input)
-                }, false)
-            })
-        });
-    
-    
-    
-        [" dragleave", "drop"].forEach(eventType => {
-            fileInputs.forEach((input) => {
-                input.addEventListener(eventType, () => {
-                    unHightLight(input)
-                }, false)
-            })
-        });
-    */
-}
+  }
 
+  ["dragenter", "dragover"].forEach((eventType) => {
+    fileInputs.forEach((input) => {
+      input.addEventListener(
+        eventType,
+        () => {
+          hightLight(input);
+        },
+        false
+      );
+    });
+  });
+  ["dragleave", "drop"].forEach((eventType) => {
+    fileInputs.forEach((input) => {
+      input.addEventListener(
+        eventType,
+        () => {
+          unHightLight(input);
+        },
+        false
+      );
+    });
+  });
 
+  fileInputs.forEach((input) => {
+    input.addEventListener("drop", (e) => {
+      input.files = e.dataTransfer.files;
+      let dots;
+      const arr = input.files[0].name.split(".");
+      arr[0].length > 6 ? (dots = "...") : (dots = ".");
+      const name = arr[0].substring(0, 6) + dots + arr[1];
+      input.previousElementSibling.textContent = name;
+    });
+  });
+};
 
-export default drop
+export default drop;
